f1437901b08ff3492deb95a898e7f192a527cddf
Laliga1 <laliga1deperu@outlook.com>
2026-05-11 15:25:05 -0500

    Update flashjs.js

--- flashjsgames/flashjs.js
+++ flashjsgames/flashjs.js
@@ -3,13 +3,13 @@ function ocultarTodo() {
     todos.forEach(el => el.style.display = 'none');
 }
 
-document.getElementById('btnconsole1').addEventListener('click', function () {
+document.getElementById('btn1').addEventListener('click', function () {
     ocultarTodo(); 
     const actuales = document.querySelectorAll('#flashgame');
     actuales.forEach(el => el.style.display = 'flex'); 
 });
 
-document.getElementById('btnconsole2').addEventListener('click', function () {
+document.getElementById('btn2').addEventListener('click', function () {
     ocultarTodo();
     const actuales = document.querySelectorAll('#jsgame');
     actuales.forEach(el => el.style.display = 'flex');
