36d8f36d179e201634ef729beb04315e26faf2eb
L1games <laliga1deperu@outlook.com>
2026-07-11 23:20:44 -0500

    Update emulator

--- computergames/amiga/scriptpage.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function(event) {
-        
-        if (event.key.toLowerCase() === 'p') {
-            
-            window.location.href = '../../index.html'; 
-        }
-    });
